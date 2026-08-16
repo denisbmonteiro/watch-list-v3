833ff5bc95a3aac3366be4ce43fd26eeaa844a33
Denis Bittencourt <denis765@gmail.com>
2026-08-16 03:55:31 -0300

    Add FileNames class for consistent file name management

---
+++ WatchList/Constants/FileNames.cs
@@ -0,0 +1,13 @@
+namespace WatchList.Constants;
+
+public static class FileNames
+{
+    public const string Anime = "anime.txt";
+    public const string Books = "books.txt";
+    public const string Games = "games.txt";
+    public const string Index = "index.txt";
+    public const string Manga = "manga.txt";
+    public const string Movies = "movies.txt";
+    public const string Queue = "queue.txt";
+    public const string Series = "series.txt";
+}
